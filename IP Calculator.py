69b42325df37877f43706a67e93943a4faf01b82
Moein Golbachi <moein.mhzg@gmail.com>
2021-06-04 22:17:26 +0430

    some modifications on functions for a better fucntion use

--- IP Calculator.py
+++ IP Calculator.py
@@ -1,6 +1,17 @@
 # IP = {'address': 'x.x.x.x', 'prefix': 'x'}
 
 
+def segmented(raw_addres):
+    segmented_address = str()
+    for i in range(8, 33, 8):
+        segmented_address += raw_addres[i - 8:i] + '.'
+    return segmented_address[:-1]
+
+
+def raw_binary(binary):
+    return binary.replace('.', '')
+
+
 def segments(address):
     return address.split('.')
 
@@ -9,34 +20,28 @@ def wildcard(prefix):
     return (32 - prefix)
 
 
-def binary_address(address):
+def binary(address):
     binary_address = str()
-    for each in segments(address):
-        raw_binary = format(int(each), 'b')
-        if not len(raw_binary) == 8:
-            raw_binary = (8 - len(raw_binary)) * '0' + raw_binary
-        binary_address += raw_binary + '.'
+    for segment in segments(address):
+        segment_binary = format(int(segment), 'b')
+        if not len(segment_binary) == 8:
+            segment_binary = (8 - len(segment_binary)) * '0' + segment_binary
+        binary_address += segment_binary + '.'
     return binary_address[:-1]
 
 
 def binary_netmask(prefix):
     raw_binary = int(prefix) * '1' + (32 - int(prefix)) * '0'
-    binary_netmask = str()
-    for i in range(8, 33, 8):
-        binary_netmask += raw_binary[i - 8:i] + '.'
-    return binary_netmask[:-1]
+    return segmented(raw_binary)
 
 
 def binary_wildcard(wildcart_prefix):
     raw_binary = (32 - int(wildcart_prefix)) * '0' + int(wildcart_prefix) * '1'
-    binary_wildcard = str()
-    for i in range(8, 33, 8):
-        binary_wildcard += raw_binary[i - 8:i] + '.'
-    return binary_wildcard[:-1]
+    return segmented(raw_binary)
 
 
 def decimal(address):
-    decimal_address = 0
-    for each in segments(address):
-        decimal_address += str(int(each, 2)) + '.'
-    return decimal_address[-1]
+    decimal_address = str()
+    for segment in segments(address):
+        decimal_address += str(int(segment, 2)) + '.'
+    return decimal_address[:-1]
